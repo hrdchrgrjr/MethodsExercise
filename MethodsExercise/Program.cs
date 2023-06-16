b9398ae85fe59f9bde11f28ccee1244f7e248ac8
hrdchrgrjr <hrdchrgrjr@gmail.com>
2023-06-16 19:29:44 -0400

    Saved imputs for favorites

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, What is your name?");
+             var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
